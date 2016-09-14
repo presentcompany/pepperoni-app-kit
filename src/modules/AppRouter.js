22ea37e82534889cb16c0c9750d0d84b0f82cf77
Ben McPhail <ben@presentcompany.co>
2016-09-14 16:10:20 +1000

    Update routes

--- src/modules/AppRouter.js
+++ src/modules/AppRouter.js
@@ -1,26 +1,17 @@
 /*eslint-disable react/prop-types*/
 
 import React from 'react';
-import CounterViewContainer from './counter/CounterViewContainer';
-import ColorViewContainer from './colors/ColorViewContainer';
-
+import HomeContainer from './home/HomeContainer';
+import MainContainer from './main/MainContainer';
 /**
  * AppRouter is responsible for mapping a navigator scene to a view
  */
-export default function AppRouter(props) {
-  const key = props.scene.route.key;
-
-  if (key === 'Counter') {
-    return <CounterViewContainer />;
+export default function AppRouter(route, navigator) {
+  if(route.name == 'Main') {
+   return <MainContainer navigator={navigator}/>
   }
-
-  if (key.indexOf('Color') === 0) {
-    const index = props.scenes.indexOf(props.scene);
-    return (
-      <ColorViewContainer
-        index={index}
-      />
-    );
+  if(route.name == 'Home') {
+   return <HomeContainer navigator={navigator}/>
   }
 
   throw new Error('Unknown navigation key: ' + key);
